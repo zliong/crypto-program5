4918d1442bbc25e935990979129b2a0a0627e214
zliong <zachary12453@gmail.com>
2022-03-03 18:10:30 -0800

    post initial website

---
+++ application.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import boto3
+import boto
+from boto3.dynamodb.conditions import Key
+from botocore.client import ClientError
+from urllib.request import urlopen
+import urllib3
+import codecs
+
+application = Flask(__name__)
+
+@application.route('/')
+def home_page():
+    return render_template("base.html")
+
+
+if __name__ == "__main__":
+    application.run()
