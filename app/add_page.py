46f2c5c5e99fea210b963bb0bd57a036ce66943d
Mo <mooaz09@gmail.com>
2022-03-04 19:09:18 -0800

    check type of button

--- app/add_page.py
+++ app/add_page.py
@@ -14,13 +14,15 @@ add_ticker = Blueprint('add_page', __name__, template_folder='templates')
 def add():
     form = AddTicker(request.form)
     if request.method == 'POST':
-        requested_ticker = form.ticker.data.strip()
-        if len(requested_ticker) != 0 and requested_ticker is not None and requested_ticker != '':
-            fetch_req = requests.get(f'https://data.messari.io/api/v1/assets/{requested_ticker}/metrics')
-            if fetch_req.status_code != 200:
-                pass  # render error
+        if request.form.get('add') == 'Add':
+            requested_ticker = form.ticker.data.strip()
+            if len(requested_ticker) != 0 and requested_ticker is not None and requested_ticker != '':
+                fetch_req = requests.get(f'https://data.messari.io/api/v1/assets/{requested_ticker}/metrics')
+                print(requested_ticker)
+                if fetch_req.status_code != 200:
+                    pass  # render error
+                else:
+                    pass  # we know that this is a valid ticker, add it to db
             else:
-                pass  # we know that this is a valid ticker, add it to db
-        else:
-            return render_template("add.html", form=form, ticker_not_filled='Please fill in the ticker field.')
+                return render_template("add.html", form=form, ticker_not_filled='Please fill in the ticker field.')
     return render_template("add.html", form=form)
