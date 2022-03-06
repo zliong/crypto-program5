c4fc44463a98b1422ecbdc1ec0c57bfa22682dba
StuartStrom <stuart.strom@live.com>
2022-03-05 17:27:40 -0800

    Add files via upload

---
+++ add_user.py
@@ -0,0 +1,31 @@
+from flask import render_template, request, Blueprint
+import requests
+import boto3
+from wtforms import Form, StringField
+
+create_user_blueprint = Blueprint('add_user', __name__, template_folder='templates') #add to blueprint
+
+
+@create_user_blueprint.route('/create_user', methods=['POST', 'GET'])
+def create_user():
+    if request.method == 'POST':
+        return redirect(url_for('home_page'))
+
+    return render_template("create_user.html", place_one='Enter email', place_two='Enter Password',
+                           place_three='Enter Username')  #add text for place holder
+
+
+@create_user_blueprint.route('/create_user_submit', methods=['POST', 'GET'])
+def create_user_submit():
+    email = request.form['text_email']  #get user inputs
+    password = request.form['text_password']
+    username = request.form['text_username']
+    if email == '' or (password == '') or (username == ''):
+        return render_template("create_user.html", place_one='fill out all fields')  # If one field is empty
+    else:  # add user to DynamoDB                                                    #notify user
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table('Program5Users')  #get table
+        response = table.put_item(
+            Item={'email': email, 'password': password, 'username': username}
+        )          #put item into table, email, password, and username
+    return render_template("create_user.html") #stay on same page
--- application.py
+++ application.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from views.add_user import create_user_blueprint
 
 application = Flask(__name__)
+application.register_blueprint(create_user_blueprint)
 
 
 @application.route('/', methods=['POST', 'GET'])
 def home_page():
     return render_template("base.html")
+
+
+def submit_user_info():   #eventually going to have this completed in this file,since it's the base view
+    email = request.form['text_email']
 
 
 if __name__ == "__main__":
