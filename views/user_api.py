8684144a10d50911124c098135b22486ed82e98a
ejcanoy <euan.james.canoy@gmail.com>
2022-03-08 10:13:07 -0800

    fixed bugs with user_api.py

--- views/user_api.py
+++ views/user_api.py
@@ -1,25 +1,31 @@
 from flask import Blueprint, jsonify
 from .core import limiter
 import boto3
+from boto3.dynamodb.conditions import Key
 
-__TableName__ = "program5"
+__TableName__ = "Program5"
 dynamodb = boto3.resource("dynamodb", region_name='us-west-2')
 table = dynamodb.Table(__TableName__)
 
-web_api_blueprint = Blueprint('user_api', __name__, template_folder='templates')
-limiter.limit("1 per day")(web_api_blueprint)
+web_api_blueprint = Blueprint(
+    'user_api', __name__, template_folder='templates')
+limiter.limit("200 per day")(web_api_blueprint)
+
 
 @web_api_blueprint.route('/api/v1/<user_email>/', methods=['GET'])
 def get_user_list(user_email):
     # get list from DB and return
-   response = table.query(
-      KeyConditionExpression=Key('email').eq(user_email)
-   )
-
-   profile = response["Items"]
-   tickers = {}
-   for attribute in profile:
-      if attribute != 'username' and attribute != 'password':
-         tickers[attribute] = "messari.io/asset/{attribute}"
+    print(user_email)
+    response = table.query(
+        KeyConditionExpression=Key('email').eq(user_email)
+    )
+    
+    profiles = response["Items"]
+    tickers = {}
+    for profile in profiles:
+        for attribute in profile:
+            print(attribute)
+            if attribute != 'email' and attribute != 'password':
+                tickers[profile[attribute]] = "messari.io/asset/" + attribute
          
-   return jsonify(tickers)
+    return jsonify(tickers)
