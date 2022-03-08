b108e86d1c6c3fa34f5d2b6bbb2a7f871c155863
ejcanoy <euan.james.canoy@gmail.com>
2022-03-07 18:02:48 -0800

    added table query

--- views/user_api.py
+++ views/user_api.py
@@ -7,4 +7,14 @@ limiter.limit("1 per day")(web_api_blueprint)
 @web_api_blueprint.route('/api/v1/<user_email>/', methods=['GET'])
 def get_user_list(user_email):
     # get list from DB and return
-    return jsonify(email=user_email)
+   response = table.query(
+      KeyConditionExpression=Key('email').eq(user_email)
+   )
+
+   profile = response["Items"]
+   tickers = {}
+   for attribute in profile:
+      if attribute != 'username' and attribute != 'password':
+         tickers[attribute] = "messari.io/asset/{attribute}"
+         
+   return jsonify(tickers)
