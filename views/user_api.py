4be582c08ce6abfbf5280d85527e6ccf62535154
ejcanoy <euan.james.canoy@gmail.com>
2022-03-07 18:09:49 -0800

    added table declaration

--- views/user_api.py
+++ views/user_api.py
@@ -1,5 +1,10 @@
 from flask import Blueprint, jsonify
 from .core import limiter
+import boto3
+
+__TableName__ = "program5"
+dynamodb = boto3.resource("dynamodb", region_name='us-west-2')
+table = dynamodb.Table(__TableName__)
 
 web_api_blueprint = Blueprint('user_api', __name__, template_folder='templates')
 limiter.limit("1 per day")(web_api_blueprint)
