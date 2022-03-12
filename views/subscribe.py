cc9b70d69e23302b9729aeb0ddd048b2c216940b
ejcanoy <euan.james.canoy@gmail.com>
2022-03-11 23:17:00 -0800

    added subscribe unsubscribe

--- views/subscribe.py
+++ views/subscribe.py
@@ -1,12 +1,18 @@
 from email import message
+from operator import sub
 from flask import Blueprint, render_template, request, redirect, url_for, session
 import boto3
 import logging
 import time
 from botocore.exceptions import ClientError
+from boto3.dynamodb.conditions import Key
 
 logger = logging.getLogger(__name__)
 sns_client = boto3.client('sns')
+__TableName__ = 'Program5'
+dynamodb = boto3.resource("dynamodb", region_name='us-west-2')
+table = dynamodb.Table(__TableName__)
+dynamodb_client = boto3.client('dynamodb')
 
 subscribe_blueprint = Blueprint(
     'subscribe', __name__, template_folder='templates')
@@ -14,29 +20,94 @@ subscribe_blueprint = Blueprint(
 
 @subscribe_blueprint.route('/subscribe', methods=['GET', 'POST'])
 def subscribe():
+    user_email = 'ejkj699@gmail.com'
     if request.method == "GET":
-        return render_template("subscribe.html")
+        
+        # user_email = session['user'] uncomment this when session is connected
+
+        # get status if they are subscribed
+        response = table.query(
+        KeyConditionExpression=Key('email').eq(user_email)
+        )
+        profiles = response["Items"]
+        # do we have to deal with there not being any account?
+        user = profiles[0]
+        subscribe = False
+        if user['subscribed'] != 'False':
+            subscribe = True
+        return render_template("subscribe.html", subscribed = subscribe)
     else:
-        # find a way to get the email from session
-        topic = 'arn:aws:sns:us-west-2:440026663620:testTopic'
-        protocol = 'Email'
-        endpoint = 'ejkj699@gmail.com'
-        try:
-            subscription = sns_client.subscribe(
-                TopicArn=topic, Protocol=protocol, Endpoint=endpoint, ReturnSubscriptionArn=True)
-            logger.info("Subscribed %s %s to topic %s.", protocol, endpoint, topic)
-            mes = "Subscribed {} {} to topic {}.".format(protocol, endpoint, topic)
-            #session['messages'] = mes
-            #return redirect("/subscribe_confirmation")
-            #return subscribe_confirmation(topic, protocol, endpoint)
-            return redirect(url_for('subscribe_confirmation', page = "name"))
-        except ClientError:
-            print("here")
-            logger.exception(
-                "Couldn't subscribe %s %s to topic %s.", protocol, endpoint, topic)
-            print("couldn't do the subscription")
+        if "subscribe" in request.form:
+                    # find a way to get the email from session
+            topic = 'arn:aws:sns:us-west-2:440026663620:testTopic'
+            protocol = 'Email'
+            endpoint = 'ejkj699@gmail.com'
+            try:
+                subscription = sns_client.subscribe(
+                    TopicArn=topic, Protocol=protocol, Endpoint=endpoint, ReturnSubscriptionArn=True)
+                print(subscription)
+                logger.info("Subscribed %s %s to topic %s.", protocol, endpoint, topic)
+                dynamodb_client.update_item(
+                    TableName = __TableName__,
+                    Key = {
+                        'email':{'S':user_email}
+                    },
+                    ExpressionAttributeNames = {
+                        '#sub':'subscribed'
+                    },
+                    UpdateExpression="set #sub = :subs",
+                    ExpressionAttributeValues={
+                        ':subs': {
+                            'S':subscription['SubscriptionArn']
+                        }
+                    }
+                )
+                mes = "Subscribed {} {} to topic {}.".format(protocol, endpoint, topic)
+                return f"""<h1> {mes} <h1>
+                            <a href="/subscribe">go back to subscribe<a>"""
+            except ClientError:
+                print("here")
+                logger.exception(
+                    "Couldn't subscribe %s %s to topic %s.", protocol, endpoint, topic)
+                print("couldn't do the subscription")
+                return "subscribed"
+        else:
+            response = table.query(
+                KeyConditionExpression=Key('email').eq(user_email)
+            )
+            profiles = response["Items"]
+            # do we have to deal with there not being any account?
+            user = profiles[0]
+            subscription_Arn = user['subscribed']
+            try:
+                sns_client.unsubscribe(SubscriptionArn=subscription_Arn)
+                dynamodb_client.update_item(
+                    TableName = __TableName__,
+                    Key = {
+                        'email':{'S':user_email}
+                    },
+                    ExpressionAttributeNames = {
+                        '#sub':'subscribed'
+                    },
+                    UpdateExpression="set #sub = :subs",
+                    ExpressionAttributeValues={
+                        ':subs': {
+                            'S':'False'
+                        }
+                    }
+                )
+
+                return """<h1> unsubscribed from arn:aws:sns:us-west-2:440026663620:testTopic <h1>
+                        <a href="/subscribe">go back to subscribe/unsubscribe page<a>"""
+            except ClientError:
+                # logger.exception("Couldn't unsubscribe to {subscription_arn}.")
+                # raise
+                return """<h1> you must confirm subscription before unsubscribing <h1>
+                            <a href="/subscribe">go back to subscribe/unsubscribe page <a>"""
+
+
 
 @subscribe_blueprint.route('/subscribe_confirmation/<page>', methods=['GET'])
-def subscribe_confirmation(page):
-    return "<h1>{}<h1>".format(page)
+def subscribe_confirmation():
+    return "<h1>{}<h1>".format()
 
